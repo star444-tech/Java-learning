be58ad3c600f1bdf70a0f3bda64485c35005feb1
fuyao_coder <2363574058@qq.com>
2026-08-26 17:33:17 +0800

    Add files via upload

---
+++ literal.java
@@ -0,0 +1,8 @@
+public class literal {
+    public static void main(String[] args) {
+        System.out.println("霸王龙" );
+        System.out.println(8 );
+        System.out.println(8.5 );
+        System.out.println('公' );
+    }
+}
---
+++ variable.java
@@ -0,0 +1,12 @@
+public class variable {
+    public static void main(String[] args) {
+        double wechatmoney = 8.5;
+        double alipaymoney = 10.0;
+        double bankmoney = 9.0;
+
+        System.out.println(wechatmoney+alipaymoney+bankmoney);
+        wechatmoney = wechatmoney + 1.5;
+        wechatmoney = wechatmoney - 2.0;
+        System.out.println(wechatmoney);
+    }
+}
